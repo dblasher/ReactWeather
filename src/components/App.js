1e3aa4d43014116a6ad84e3485735ca8e18106f0
dblasher <dblasher90@gmail.com>
2018-06-04 19:19:49 -0700

    connected zipcode submit to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import ZipForm from './ZipForm';
 
 class App extends Component {
 constructor(props){
@@ -11,22 +12,26 @@ constructor(props){
       dates: [],
       selectedDate: null
     };
+    this.url = "http://api.openweathermap.org/data/2.5/forecast/daily?zip=";
+    this.apikey = "&units=imperial&appid=4319ff27021579d6c2adf5fc9c83b5eb";
+
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+
   }//end of constructor
+
+  onFormSubmit(zipcode){
+    this.setState( {zipcode} );
+  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       This is my weather app. we'll use {this.url} for getting weather data.
+       <ZipForm onSubmit = {this.onFormSubmit}/>
       </div>
+      
     );
   }
-
 
 } //end of App class
 
